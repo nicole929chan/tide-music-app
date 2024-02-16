fad9ad561ac3ee09bb2cbdc6edaade79fe1eccab
Nicole <nuling.huang@ehsn.com.tw>
2024-02-16 15:57:39 +0800

    s3初始化不指定instance profile

--- app/Services/AwsService.php
+++ app/Services/AwsService.php
@@ -1,22 +1,29 @@
 <?php
 namespace App\Services;
 
+use Aws\S3\S3Client;
+
 class AwsService
 {
+    protected $s3Client;
     public function __construct()
     {
-        // ...
+        $this->s3Client = new S3Client([
+            'version' => 'latest',
+            'region'  => 'ap-southeast-2'
+        ]);
     }
 
     public function getBucketList()
     {
         // Get-pices is a instance profile 
-        $s3Client = new \Aws\S3\S3Client([
-            'profile' => 'Get-pics',
-            'version' => 'latest',
-            'region'  => 'ap-southeast-2'
-        ]);
+        // $s3Client = new \Aws\S3\S3Client([
+        //     'profile' => 'Get-pics',
+        //     'version' => 'latest',
+        //     'region'  => 'ap-southeast-2'
+        // ]);
         
-        return $s3Client->listBuckets();
+        // return $s3Client->listBuckets();
+        return $this->s3Client->listBuckets();
     }
 }
